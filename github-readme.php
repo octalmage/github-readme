c3c17a09fcdbb561a15a52d240d5b61f65607263
Ian M. Jones <ian@ianmjones.com>
2015-09-21 16:13:00 +1200

    Formatting cleanup.

--- github-readme.php
+++ github-readme.php
@@ -16,107 +16,117 @@ add_shortcode( 'github_readme', 'github_readme_func' );
 add_shortcode( 'github_markdown', 'github_markdown_func' );
 add_shortcode( 'github_wikipage', 'github_wikipage_func' );
 
-function github_readme_func( $atts )
-{
-    extract( shortcode_atts( array(
-        'repo' => 'octalmage/GitHub Shortcode',
-        'trim' => 0
-    ), $atts ) );
+function github_readme_func( $atts ) {
+	extract(
+		shortcode_atts(
+			array(
+				'repo' => 'octalmage/GitHub Shortcode',
+				'trim' => 0,
+			),
+			$atts
+		)
+	);
 
-    $transient="github_readme_" . $repo . "_" . $trim;
-    if ( false === ( $html = get_transient($transient)))
-    {
-        $url="https://api.github.com/repos/" . $repo . "/readme";
+	$transient = "github_readme_" . $repo . "_" . $trim;
 
-        $ch = curl_init();
-        $timeout = 5;
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_USERAGENT,'WordPress');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-        $data = curl_exec($ch);
-        curl_close($ch);
+	if ( false === ( $html = get_transient( $transient ) ) ) {
+		$url = "https://api.github.com/repos/" . $repo . "/readme";
 
-        $json=json_decode($data);
-        $markdown=base64_decode($json->content);
-        if ($trim>0)
-        {
-            $markdown = implode("\n", array_slice(explode("\n", $markdown), $trim));
-        }
+		$ch      = curl_init();
+		$timeout = 5;
+		curl_setopt( $ch, CURLOPT_URL, $url );
+		curl_setopt( $ch, CURLOPT_USERAGENT, 'WordPress' );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+		$data = curl_exec( $ch );
+		curl_close( $ch );
 
-        $html = Markdown::defaultTransform($markdown);
-        set_transient($transient, $html , 12 * HOUR_IN_SECONDS);
-    }
-    return $html;
+		$json     = json_decode( $data );
+		$markdown = base64_decode( $json->content );
+		if ( $trim > 0 ) {
+			$markdown = implode( "\n", array_slice( explode( "\n", $markdown ), $trim ) );
+		}
+
+		$html = Markdown::defaultTransform( $markdown );
+		set_transient( $transient, $html, 12 * HOUR_IN_SECONDS );
+	}
+
+	return $html;
 }
 
-function github_markdown_func( $atts )
-{
-    extract( shortcode_atts( array(
-        'repo' => 'octalmage/GitHub Shortcode',
-        'trim' => 0,
-        'cache' => 60,
-        'file' => '/readme',
-        'branch' => 'master'
-    ), $atts ) );
+function github_markdown_func( $atts ) {
+	extract(
+		shortcode_atts(
+			array(
+				'repo'   => 'octalmage/GitHub Shortcode',
+				'trim'   => 0,
+				'cache'  => 60,
+				'file'   => '/readme',
+				'branch' => 'master',
+			),
+			$atts
+		)
+	);
 
-    $transient="github_markdown_" . $repo . "_" . $file . "_" . $trim;
-    if ( false === ( $html = get_transient($transient)))
-    {
-        $url="https://raw.githubusercontent.com/" . $repo . "/" . $branch . "/" . $file;
+	$transient = "github_markdown_" . $repo . "_" . $file . "_" . $trim;
 
-        $ch = curl_init();
-        $timeout = 5;
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_USERAGENT,'WordPress');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-        $markdown = curl_exec($ch);
-        curl_close($ch);
+	if ( false === ( $html = get_transient( $transient ) ) ) {
+		$url = "https://raw.githubusercontent.com/" . $repo . "/" . $branch . "/" . $file;
 
-        if ($trim>0)
-        {
-            $markdown = implode("\n", array_slice(explode("\n", $markdown), $trim));
-        }
+		$ch      = curl_init();
+		$timeout = 5;
+		curl_setopt( $ch, CURLOPT_URL, $url );
+		curl_setopt( $ch, CURLOPT_USERAGENT, 'WordPress' );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+		$markdown = curl_exec( $ch );
+		curl_close( $ch );
 
-        $html = Markdown::defaultTransform($markdown);
-        set_transient($transient, $html, $cache);
-    }
-    return $html;
+		if ( $trim > 0 ) {
+			$markdown = implode( "\n", array_slice( explode( "\n", $markdown ), $trim ) );
+		}
+
+		$html = Markdown::defaultTransform( $markdown );
+		set_transient( $transient, $html, $cache );
+	}
+
+	return $html;
 }
 
-function github_wikipage_func( $atts )
-{
-    extract( shortcode_atts( array(
-        'repo' => 'octalmage/GitHub Shortcode',
-        'trim' => 0,
-        'cache' => 60,
-        'page' => ''
-    ), $atts ) );
+function github_wikipage_func( $atts ) {
+	extract(
+		shortcode_atts(
+			array(
+				'repo'  => 'octalmage/GitHub Shortcode',
+				'trim'  => 0,
+				'cache' => 60,
+				'page'  => '',
+			),
+			$atts
+		)
+	);
 
-    $transient="github_wikipage_" . $repo . "_" . $page;
-    if ( false === ( $html = get_transient($transient)))
-    {
-        $url="https://raw.githubusercontent.com/wiki/" . $repo . "/" . $page . ".md";
+	$transient = "github_wikipage_" . $repo . "_" . $page;
 
-        $ch = curl_init();
-        $timeout = 5;
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_USERAGENT,'WordPress');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-        $markdown = curl_exec($ch);
-        curl_close($ch);
+	if ( false === ( $html = get_transient( $transient ) ) ) {
+		$url = "https://raw.githubusercontent.com/wiki/" . $repo . "/" . $page . ".md";
 
-        if ($trim>0)
-        {
-            $markdown = implode("\n", array_slice(explode("\n", $markdown), $trim));
-        }
+		$ch      = curl_init();
+		$timeout = 5;
+		curl_setopt( $ch, CURLOPT_URL, $url );
+		curl_setopt( $ch, CURLOPT_USERAGENT, 'WordPress' );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+		$markdown = curl_exec( $ch );
+		curl_close( $ch );
 
-        $html = Markdown::defaultTransform($markdown);
-        set_transient($transient, $html, $cache);
-    }
-    return $html;
+		if ( $trim > 0 ) {
+			$markdown = implode( "\n", array_slice( explode( "\n", $markdown ), $trim ) );
+		}
+
+		$html = Markdown::defaultTransform( $markdown );
+		set_transient( $transient, $html, $cache );
+	}
+
+	return $html;
 }
-
-?>
